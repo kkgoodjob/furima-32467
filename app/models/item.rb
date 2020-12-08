f364348faaa7da45901a84ee9065421b4af43b5d
kkgoodjob <70433180+kkgoodjob@users.noreply.github.com>
2020-12-08 18:20:42 +0900

    修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
-  validates :name, :image, :explanation, presence: true
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
@@ -10,7 +9,7 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_fee_burden
 
-  validates :category, :condition, :days_to_ship, :prefecture, :shipping_fee_burden, presence: true
+  validates :name, :image, :explanation, :category, :condition, :days_to_ship, :prefecture, :shipping_fee_burden, presence: true
   validates :price, format: { with: /\A[0-9]+\z/ }
   validates :category_id, :condition_id, :days_to_ship_id, :prefecture_id, :shipping_fee_burden_id, numericality: { other_than: 1 }
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
