788bd35ecf495c6cada8c84812b0adc31979bec9
kkgoodjob <70433180+kkgoodjob@users.noreply.github.com>
2020-12-15 19:07:09 +0900

    修正２

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,9 +3,7 @@ class OrdersController < ApplicationController
   before_action :find_item, only: [:create, :index]
   def index
     @order_address = OrderAddress.new
-    if user_signed_in? && current_user.id != @order_item.user_id
-        redirect_to root_path and return if @order_item.id == @order_item.order.item_id
-    else
+    if current_user.id == @order_item.user_id || @order_item.id == @order_item.order.item_id
       redirect_to root_path and return
     end
   end
